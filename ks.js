4d410533b4c1bd8052a5c11e9c660add01886fd7
ah3548@nyu.edu <ah3548@nyu.edu>
2016-04-17 18:40:17 +0000

    Added endpoint for wikipedia

--- ks.js
+++ ks.js
@@ -1,14 +1,18 @@
 var express = require('express');
+var wiki = require('./ks-wiki');
 var app = express();
 
 //var Faker = require('faker');
 //var randomName = Faker.Name.findName(); // Rowan Nikolaus containing many properties
+    console.log(wiki);
 
 app.use(express.static('.'));
 
-/*app.get('/', function (req, res) {
-  res.send('Hello ' + randomName + '!');
-});*/
+app.get('/wiki', function (req, res) {
+    wiki(function(response) {
+        res.send(response);
+    });
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
