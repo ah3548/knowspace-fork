2b582224b8d0a20c12ef47f902612f7a7ef09a61
ah3548@nyu.edu <ah3548@nyu.edu>
2016-04-18 01:34:22 +0000

    integrated orm and wikipedia feature at endpoint

--- ks.js
+++ ks.js
@@ -1,20 +1,53 @@
 var express = require('express');
 var wiki = require('./ks-wiki');
+var orm = require('./ks-orm');
 var app = express();
+
 
 //var Faker = require('faker');
 //var randomName = Faker.Name.findName(); // Rowan Nikolaus containing many properties
-    console.log(wiki);
+
+
+var subject = "Linear_algebra";
 
 app.use(express.static('.'));
 
+
+function getWikiFromDB(subject) {
+    return orm.getWikiEntry(subject).then(
+        function(content) {
+            return content;
+        }
+    ).catch(
+        function(error) {
+            console.log("Wikipedia Entry not found, fetching now..");
+            return getWiki(subject).then(
+                function(content) {
+                    return content;
+                }
+            );
+        }
+    );
+}
+
+function getWiki(subject) {
+    return wiki.getWikiEntry(subject).then(
+        function(content) {
+            return orm.createWikiEntry( 
+            {subject:subject, content:content});
+        }
+    );
+}
+
+
 app.get('/wiki', function (req, res) {
-    wiki(function(response) {
-        res.send(response);
-    });
+    getWikiFromDB(subject).then(
+        function(content) {
+            res.send(content);
+        }
+    );
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Listening on port 3000!');
 });
-
