67a04c04eb6412e1f8b6c625823a92b487fb80fd
A&H <ah3548@nyu.edu>
2017-05-02 22:55:45 -0400

    fixing bug

--- ks/js/app.js
+++ ks/js/app.js
@@ -206,8 +206,8 @@ angular.module('ksApp', ['ngResource', 'angular-bind-html-compile', 'ngCookies']
             }
 
             $scope.appIntercept = function (linkPath) {
-                var rPath = linkPath.split('/').replace('_',' ');
-                addSub(rPath[2]);
+                var rPath = linkPath.split('/');
+                addSub(rPath[2].replace('_',' '));
             }
 
             // ################### SERVICES ############################## 
